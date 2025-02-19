b136df7b16b9756aba5861e80b9d4601714d1200
haohmaru3000 <tuanit168@gmail.com>
2025-02-19 12:37:25 +0700

    5. Config for swagger doc with some initial information

---
+++ employee_service/src/main/java/com/ion/employee_service/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.ion.employee_service.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Employee Api Specification - ION Mobility",
+                description = "Api documentation for Employee Service",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Thomas Ng",
+                        email = "tuanit@outlook.com",
+                        url = "https://ion.vercel.app"
+                ),
+                license = @License(
+                        name = "MIT License",
+                        url = "https://ion.vercel.app/licenses"
+                ),
+                termsOfService = "https://ion.vercel.app/terms"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:9002"
+                ),
+                @Server(
+                        description = "Dev ENV",
+                        url = "https://employee-service.dev.com"
+                ),
+                @Server(
+                        description = "Prod ENV",
+                        url = "https://employee-service.prod.com"
+                ),
+        }
+)
+public class OpenApiConfig {
+}
