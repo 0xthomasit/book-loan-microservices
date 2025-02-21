0a8af6c6efa304f790dd0a4e37c800c83b685d66
haohmaru3000 <tuanit168@gmail.com>
2025-02-21 10:36:34 +0700

    7. Config Apache-Kafka at Common service

--- book_service/src/main/java/com/ion/book_service/query/controller/BookQueryController.java
+++ book_service/src/main/java/com/ion/book_service/query/controller/BookQueryController.java
@@ -3,13 +3,11 @@ package com.ion.book_service.query.controller;
 import com.ion.book_service.query.model.BookResponseModel;
 import com.ion.book_service.query.queries.GetAllBookQuery;
 import com.ion.book_service.query.queries.GetBookDetailQuery;
+import com.ion.common_service.services.KafkaService;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +17,18 @@ public class BookQueryController {
     @Autowired
     private QueryGateway queryGateway;
 
+    @Autowired
+    private KafkaService kafkaService;
+
     @GetMapping
     public List<BookResponseModel> getAllBooks() {
         GetAllBookQuery query = new GetAllBookQuery();
         return queryGateway.query(query, ResponseTypes.multipleInstancesOf(BookResponseModel.class)).join();
+    }
+
+    @PostMapping("/sendMessage")
+    public void sendMessage(@RequestBody String message) {
+        kafkaService.sendMessage("test", message);
     }
 
     @GetMapping("{bookId}")
