6a9e2c4b753eeb8bd73b45e9a22c02256e22654e
haohmaru3000 <tuanit168@gmail.com>
2025-02-22 12:26:24 +0700

    8. Create command, event, model for borrowing_service

---
+++ borrowing_service/src/main/java/com/ion/borrowing_service/command/model/BorrowingCreateModel.java
@@ -0,0 +1,18 @@
+package com.ion.borrowing_service.command.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BorrowingCreateModel {
+
+    private String bookId;
+    private String employeeId;
+    private Date borrowingDate;
+
+}
